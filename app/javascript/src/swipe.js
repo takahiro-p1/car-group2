16b9486ab095a2713cebc032c29508225db65784
Taniguchi <takahirotakumi1@gmail.com>
2021-03-07 16:17:47 +0900

    f-swipe

--- app/javascript/src/swipe.js
+++ app/javascript/src/swipe.js
@@ -13,6 +13,10 @@ if (location.pathname == "/users") {
         card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
         card.style.opacity = (10 - index) / 10;
       });
+
+      if (newCards.length == 0) {
+        $(".no-user").addClass("is-active");
+      }
     }
 
     initCards();
@@ -35,7 +39,59 @@ if (location.pathname == "/users") {
 
         event.target.style.transform = 'translate(' + event.deltaX + 'px, ' + event.deltaY + 'px) rotate(' + rotate + 'deg)';
       });
+      hammertime.on('panend', function (event) {
+        el.classList.remove('moving');
+        swipeContainer.classList.remove('swipe_like');
+        swipeContainer.classList.remove('swipe_dislike');
+
+        let moveOutWidth = document.body.clientWidth;
+
+        let keep = Math.abs(event.deltaX) < 200
+        event.target.classList.toggle('removed', !keep);
+
+        if (keep) {
+          event.target.style.transform = '';
+        } else {
+          let endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth) + 100;
+          let toX = event.deltaX > 0 ? endX : -endX;
+          let endY = Math.abs(event.velocityY) * moveOutWidth;
+          let toY = event.deltaY > 0 ? endY : -endY;
+          let xMulti = event.deltaX * 0.03;
+          let yMulti = event.deltaY / 80;
+          let rotate = xMulti * yMulti;
+
+          event.target.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + rotate + 'deg)';
+
+          initCards();
+        }
+      });
     });
 
+    function createButtonListener(reaction) {
+      let cards = document.querySelectorAll('.swipe--card:not(.removed)');
+
+      if (!cards.length) return false;
+
+      let moveOutWidth = document.body.clientWidth * 2;
+
+      let card = cards[0];
+      card.classList.add('removed');
+
+      if (reaction == "like") {
+        card.style.transform = 'translate(' + moveOutWidth + 'px, -100px) rotate(-30deg)';
+      } else {
+        card.style.transform = 'translate(-' + moveOutWidth + 'px, -100px) rotate(30deg)';
+      }
+
+      initCards();
+    }
+
+    $('#like').on('click', function () {
+      createButtonListener("like");
+    })
+
+    $('#dislike').on('click', function () {
+      createButtonListener("dislike");
+    })
   });
 }
