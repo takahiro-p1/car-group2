e6121c80ec0e7ca6e7e832ae0e051a549118837c
Taniguchi <takahirotakumi1@gmail.com>
2021-03-08 22:54:59 +0900

    f-12-6-2

--- app/channels/chat_room_channel.rb
+++ app/channels/chat_room_channel.rb
@@ -1,13 +1,12 @@
 class ChatRoomChannel < ApplicationCable::Channel
   def subscribed
-     stream_from "some_channel"
+    # stream_from "some_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak(data)
-    ActionCable.server.broadcast 'chat_room_channel', chat_message: data['chat_message']
+  def speak
   end
 end
--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -1,6 +1,6 @@
 import consumer from "./consumer"
 
-const appChatRoom = consumer.subscriptions.create("ChatRoomChannel", {
+consumer.subscriptions.create("ChatRoomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
   },
@@ -10,21 +10,10 @@ const appChatRoom = consumer.subscriptions.create("ChatRoomChannel", {
   },
 
   received(data) {
-    return alert(data['chat_message'])
     // Called when there's incoming data on the websocket for this channel
   },
 
-  speak: function(chat_message) {
-    return this.perform('speak',  { chat_message: chat_message });
+  speak: function() {
+    return this.perform('speak');
   }
 });
-
-if(/chat_rooms/.test(location.pathname)) {
-  $(document).on("keydown", ".chat-room__message-form_textarea", function(e) {
-    if (e.key === "Enter") {
-      appChatRoom.speak(e.target.value);
-      e.target.value = '';
-      e.preventDefault();
-    }
-  })
-}
